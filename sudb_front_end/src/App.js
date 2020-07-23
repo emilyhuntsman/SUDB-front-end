894646fabf62a8b60579edf725400240a97d64f4
Emily Huntsman <emilyhuntsman@MacBook-Pro-8.local>
2020-07-23 14:45:22 -0700

    finished show page and router

--- sudb_front_end/src/App.js
+++ sudb_front_end/src/App.js
@@ -5,7 +5,6 @@ import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import Home from './components/Home'
 import Show from './components/Show'
-import Registration from "./components/Registration";
 
 
 const baseURL = "http://localhost:3003";
@@ -14,70 +13,74 @@ class App extends Component {
   state = {
     users: [],
     redirect: false,
+    bookSearch: '',
   }
 
   handleRedirect = () => {
     this.setState({ redirect: !this.state.redirect });
   }
 
-  // for users in API post auth -----------------
-
-  componentDidMount(){
-    this.getUsers();
+  handleSearch = (title) => {
+    this.setState({ bookSearch: title, redirect: true })
   }
 
-  handleAddUser = (user) => {
-    const copyUsers = [...this.state.users];
-    copyUsers.unshift(user);
-    this.setState({
-      users: copyUsers,
-      username: "",
-      password: "",
-      read: [],
-      toRead: [],
-      genres: [],
-    });
-  };
+  // for users in API post auth -----------------
 
-  getUsers = () => {
-    fetch(baseURL + "/users")
-      .then(
-        (data) => {
-          return data.json();
-        },
-        (err) => console.log(err)
-      )
-      .then(
-        (parsedData) => this.setState({ users: parsedData }),
-        (err) => console.log(err)
-      );
-  };
+  // componentDidMount(){
+  //   this.getUsers();
+  // }
 
-  deleteUser = (id) => {
-    fetch(baseURL + "/users/" + id, {
-      method: "DELETE",
-      body: JSON.stringify({}),
-      headers: {
-          'Content-Type': 'application/json'
-      }
-      }).then(() => {
-        this.componentDidMount();
-      })
-      .catch((error) => console.error({ Error: error }));
-  };
+  // handleAddUser = (user) => {
+  //   const copyUsers = [...this.state.users];
+  //   copyUsers.unshift(user);
+  //   this.setState({
+  //     users: copyUsers,
+  //     username: "",
+  //     password: "",
+  //     read: [],
+  //     toRead: [],
+  //     genres: [],
+  //   });
+  // };
+
+  // getUsers = () => {
+  //   fetch(baseURL + "/users")
+  //     .then(
+  //       (data) => {
+  //         return data.json();
+  //       },
+  //       (err) => console.log(err)
+  //     )
+  //     .then(
+  //       (parsedData) => this.setState({ users: parsedData }),
+  //       (err) => console.log(err)
+  //     );
+  // };
+
+  // deleteUser = (id) => {
+  //   fetch(baseURL + "/users/" + id, {
+  //     method: "DELETE",
+  //     body: JSON.stringify({}),
+  //     headers: {
+  //         'Content-Type': 'application/json'
+  //     }
+  //     }).then(() => {
+  //       this.componentDidMount();
+  //     })
+  //     .catch((error) => console.error({ Error: error }));
+  // };
 
   // end of user section ----------------------
 
   render() {
     return (
       <div className="container">
-        <Registration baseURL={baseURL} />
         <Header />
         <BrowserRouter>
           <Switch>
             <Route exact path="/" 
-            render={() => <Home baseURL={baseURL}/>}/>
-            <Route exact path="/book/" render={() => <Show bookID={""}/>}/>
+            render={() => <Home redirect={this.state.redirect} baseURL={baseURL} handleSearch={(title) => this.handleSearch(title)} />}/>
+            <Route exact path="/book/" render={() => <Show bookSearch={this.state.bookSearch}/>}/>
           </Switch>
         </BrowserRouter>
         <Footer />
