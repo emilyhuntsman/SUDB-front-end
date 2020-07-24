04497e23d8da3fab412a06e2a7a3b7f3fef3b8f3
Emily Huntsman <emilyhuntsman@MacBook-Pro-8.local>
2020-07-24 10:20:06 -0700

    setting up modal

--- sudb_front_end/src/components/BookOptions.jsx
+++ sudb_front_end/src/components/BookOptions.jsx
@@ -1,12 +1,31 @@
 import React, { Component } from 'react'
+import ReactModal from 'react-modal'
 
 export default class BookOptions extends Component {
+    state = {
+        showModal: false,
+    }
+
+    openModal = () => {
+        this.setState({showModal: true});
+    }
+
+    closeModal = () => {
+        this.setState({showModal: false});
+    }
+
     render() {
         return (
             <div>
                 <div className='book'>
                     <p>{this.props.books.results.books[Math.floor(Math.random() * (this.props.books.num_results - 1))].description}</p>
                 </div>
+                <button onClick={this.openModal}>Trigger Modal</button>
+                <ReactModal
+                    isOpen={this.state.showModal}
+                    contentLabel="Book Modal"> 
+                    <button onClick={() => this.closeModal()}>close</button>
+                </ReactModal>
             </div>
         )
     }
