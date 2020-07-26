40f83aa56d1175772d66e355dce18e1619d2c527
Emily Huntsman <emilyhuntsman@MacBook-Pro-8.local>
2020-07-25 20:13:11 -0700

    add functions to remove from both MyLists, buttons on show pages add to lists, or move from one list to another

--- sudb_front_end/src/components/MyLists.jsx
+++ sudb_front_end/src/components/MyLists.jsx
@@ -1,25 +1,30 @@
 import React, {Component} from 'react';
 
 class MyLists extends Component {
+
     render() {
     return (
-        <>
-            <h2>To Read</h2>
-            {this.props.futureBooks.map(book => (
-                <div className="book-div"key={book}>
-                    <p>{book}</p>
-                    <button>x</button>
-                </div>
-            ))}
-            <h2>Already Read</h2>
-            {this.props.pastBooks.map(book => (
-                <div className="book-div"key={book}>
-                    <p>{book}</p>
-                    <button>x</button>
+        <div className="list-div">
+            <div className="one-list">
+                <h2>To Read</h2>
+                {this.props.futureBooks.map(book => (
+                    <div className="book-div"key={book}>
+                        <p>{book}</p>
+                        <button onClick={() => this.props.removeBookFuture(book)}>x</button>
+                        <button onClick={() => this.props.moveBookToFuture(book)}>{"->"}</button>
+                    </div>
+                ))}
             </div>
-            ))}
-
-        </>
+            <div className="one-list">
+                <h2>Already Read</h2>
+                {this.props.pastBooks.map(book => (
+                    <div className="book-div"key={book}>
+                        <p>{book}</p>
+                        <button onClick={() => this.props.removeBookPast(book)}>x</button>
+                </div>
+                ))}
+            </div>
+        </div>
     )}
 }
 
