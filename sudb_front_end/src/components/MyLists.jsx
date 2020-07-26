5c6e9a57002d0806f153f2a733919eae53142708
Emily Huntsman <emilyhuntsman@MacBook-Pro-8.local>
2020-07-25 17:29:27 -0700

    adding removal buttons for list items

--- sudb_front_end/src/components/MyLists.jsx
+++ sudb_front_end/src/components/MyLists.jsx
@@ -6,15 +6,17 @@ class MyLists extends Component {
         <>
             <h2>To Read</h2>
             {this.props.futureBooks.map(book => (
-                <p key={book}>
-                    {book}
-                </p>
+                <div className="book-div"key={book}>
+                    <p>{book}</p>
+                    <button>x</button>
+                </div>
             ))}
             <h2>Already Read</h2>
             {this.props.pastBooks.map(book => (
-                <p key={book}>
-                    {book}
-                </p>
+                <div className="book-div"key={book}>
+                    <p>{book}</p>
+                    <button>x</button>
+            </div>
             ))}
 
         </>
